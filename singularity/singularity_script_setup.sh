4505259475bbd61359a7915aa05b64446344cb8d
MAPE-15 <matus.pestun15@gmail.com>
2026-04-06 18:49:07 +0200

    added singularity setup files

---
+++ singularity/singularity_script_setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "==== Host and kernel ===="
+uname -a
+
+echo ""
+echo "==== Memory usage ======="
+free -h
+
+echo ""
+echo "==== CPU info ==========="
+lscpu | grep -Ev '^(Flags:|Vulnerability)'
+
+echo ""
+echo "==== GPU info ==========="
+module load nvidia
+nvidia-smi
+
+echo ""
+echo "==== Running program ===="
+
+cd /workspace
+python3 ./src/main.py
