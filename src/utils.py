05b28e8500c4078866630e29d45879383122e47c
Samuel Kiszka <samuel.kiszka26@gmail.com>
2026-04-18 21:48:10 +0200

    Split validation on query, gallery, change training data on the fly

--- src/utils.py
+++ src/utils.py
@@ -2,33 +2,19 @@ import os
 
 from src.env_vars import INTERN_AUTHORS_FILE_PATH
 
-
-def prepare_output_directory(name):
-    if not os.path.exists(name):
-        os.makedirs(name)
-
-
-def read_line_file(line_file_path=INTERN_AUTHORS_FILE_PATH):
-    """
-        Read the line file and return author-file mappings.
-
-        :param line_file_path: Path to the line file.
-        :type line_file_path: str
-
-        :return: data
-        :rtype: tuple[dict[int, list[str]], list[int]]
-
-            data: Mapping of author IDs to lists of file names.
-            authors: Sorted list of unique author IDs found in the file.
-        """
-    data = dict()
-    authors = []
-    with open(line_file_path, "r", encoding="utf-8") as f:
-        for line in f:
-            file, author_id, *_ = line.split()
-            authors.append(int(author_id))
-            data[int(author_id)] = data.get(int(author_id), []) + [file]
-
-    authors = list(set(authors))
-    authors.sort()
-    return data, authors
+def convert_sec_to_hours_minutes_seconds(sec):
+    # if sec is double, convert to int
+    if isinstance(sec, float):
+        sec = int(sec)
+    hours = sec // 3600
+    minutes = (sec % 3600) // 60
+    seconds = sec % 60
+    # if hours is 0, return minutes and seconds only
+    ret_str = ""
+    if hours > 0:
+        ret_str += f"{hours}h "
+    if minutes > 0:
+        ret_str += f"{minutes}m "
+    if seconds > 0:
+        ret_str += f"{seconds}s"
+    return ret_str.strip()
